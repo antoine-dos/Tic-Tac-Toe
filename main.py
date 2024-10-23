7ca9e08c416a59a2a8d1b42455ab5833d0271b5e
Antoine <antoine.dos-santos@laplateforme.io>
2024-10-23 17:33:29 +0200

    amélioration

--- main.py
+++ main.py
@@ -1,55 +1,36 @@
 MaListe = [" ", " ", " ", " ", " ", " "," ", " ", " "]
 
+print("Que le sort vous soit favorable : ")
+
 def Grille():
     print('-------------\n'
-      '|', MaListe[6], '|', MaListe[7], '|', MaListe[8], '|\n'
-      '-------------\n'
-      '|', MaListe[3], '|', MaListe[4], '|', MaListe[5], '|\n'
-      '-------------\n'
-      '|', MaListe[0], '|', MaListe[1], '|', MaListe[2], '|\n'
-      '-------------')
+          '|', MaListe[6], '|', MaListe[7], '|', MaListe[8], '|\n'
+          '-------------\n'
+          '|', MaListe[3], '|', MaListe[4], '|', MaListe[5], '|\n'
+          '-------------\n'
+          '|', MaListe[0], '|', MaListe[1], '|', MaListe[2], '|\n'
+          '-------------')
     return Grille
 
 Joueur_X = "X"
 Joueur_O = "O"
 
-#def condition_X():
-#       if MaListe[0] == Joueur_X and MaListe[1] == MaListe[0] == MaListe[2]:
-#            print("Le gagnant est le joueur ", Joueur_X)
-#           return Fin == True
-#       
-#       elif MaListe[0] == Joueur_X and MaListe[3] == MaListe[0] == MaListe[6]:
-#           print("Le gagnant est le joueur ", Joueur_X)
-#            return Fin == True
-#        
-#        elif MaListe[0] == Joueur_X and MaListe[4] == MaListe[0] == MaListe[8]:
-#            print("Le gagnant est le joueur ", Joueur_X)
-#            return Fin == True
-#        
-#        elif MaListe[8] == Joueur_X and MaListe[5] == MaListe[8] == MaListe[2]:
-#            print("Le gagnant est le joueur ", Joueur_X)
-#            return Fin == True
-#        
-#        elif MaListe[8] == Joueur_X and MaListe[7] == MaListe[8] == MaListe[6]:
-#            print("Le gagnant est le joueur ", Joueur_X)
-#            return Fin == True
-#        
-#        elif MaListe[6] == Joueur_X and MaListe[4] == MaListe[6] == MaListe[2]:
-#            print("Le gagnant est le joueur ", Joueur_X)
-#            return Fin == True
-           
-
-Grille()
-
 Fin = False
+            
+Grille()
 
 while Fin == False:
 
     chiffre = int(input("Choisir une case : ")) -1
-    MaListe[chiffre] = Joueur_X
+
+    if MaListe[chiffre] != ' ':
+        chiffre = int(input("Choisir une autre case : ")) -1
+        MaListe[chiffre] = Joueur_X
+    else:
+        MaListe[chiffre] = Joueur_X
 
     Grille()
-    
+
     if MaListe[0] == Joueur_X and MaListe[1] == MaListe[0] == MaListe[2]:
         print("Le gagnant est le joueur ", Joueur_X)
         Fin = True
@@ -78,13 +59,17 @@ while Fin == False:
         print("Aucun gagnant")
         Fin = True
         break
-    
         
     chiffre = int(input("Choisir une case : ")) -1
-    MaListe[chiffre] = Joueur_O
+        
+    if MaListe[chiffre] != ' ':
+        chiffre = int(input("Choisir une autre case : ")) -1
+        MaListe[chiffre] = Joueur_O
+    else:
+        MaListe[chiffre] = Joueur_O
 
     Grille()
-    
+        
     if MaListe[0] == Joueur_O and MaListe[1] == MaListe[0] == MaListe[2]:
         print("Le gagnant est le joueur ", Joueur_O)
         Fin = True
@@ -113,4 +98,5 @@ while Fin == False:
         print("Aucun gagnant")
         Fin = True
         break
-    
+
+
